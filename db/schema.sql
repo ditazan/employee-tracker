621c269561b165238b954b3d1c3f6db9dd8f9bc0
dit <ditazanelli@gmail.com>
2022-02-20 12:07:38 -0600

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS departments;
+
+DROP TABLE IF EXISTS roles;
+
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE deparments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER,
+    
+    CONSTRAINT fk_department 
+    FORGEIN KEY (department_id) 
+    REFERENCES deparments(id) 
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+
+
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+
+);
